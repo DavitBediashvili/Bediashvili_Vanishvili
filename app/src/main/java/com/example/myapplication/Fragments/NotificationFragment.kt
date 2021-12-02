27c7600dd24d5fe1feec5723853b6a154a44fcd4
davitbediashvili <davit.bediashvili.1@btu.edu.ge>
2021-12-02 21:17:38 +0400

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/Fragments/NotificationFragment.kt
@@ -0,0 +1,9 @@
+package com.example.myapplication.Fragments
+
+import androidx.fragment.app.Fragment
+import com.example.myapplication.R
+
+class NotificationFragment : Fragment(R.layout.fragment_notification) {
+
+
+}
