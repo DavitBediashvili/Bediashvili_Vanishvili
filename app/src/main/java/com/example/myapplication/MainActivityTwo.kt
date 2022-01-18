9725e23494c22acb629b85c4da45d04e95e5cf50
davitbediashvili <davit.bediashvili.1@btu.edu.ge>
2022-01-18 15:25:53 +0400

    TheNew5.0(საღოლ ზუკა)

--- app/src/main/java/com/example/myapplication/MainActivityTwo.kt
+++ app/src/main/java/com/example/myapplication/MainActivityTwo.kt
@@ -3,32 +3,68 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.ActionBar
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.myapplication.FragmentsSecond.HomeFragment
+import com.example.myapplication.FragmentsSecond.NotificationsFragment
+import com.example.myapplication.FragmentsSecond.ProfileFragment
+import com.example.myapplication.FragmentsSecond.SearchFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivityTwo : AppCompatActivity() {
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.nav_home -> {
+                moveToFragments(HomeFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_search -> {
+                moveToFragments(SearchFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_add_post -> {
+
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_notifications -> {
+                moveToFragments(NotificationsFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_profile -> {
+                moveToFragments(ProfileFragment())
+                return@OnNavigationItemSelectedListener true
+
+
+            }
+        }
+
+
+        false
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_two)
+        setContentView(R.layout.activity_main)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+        moveToFragments(HomeFragment())
 
-        val actionBar: ActionBar? = supportActionBar
-        actionBar?.hide()
+    }
 
-        val navView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-        val controller = findNavController(R.id.nav_host_fragment)
-        val appBarConfig = AppBarConfiguration(
-            setOf(
-                R.id.homeFragment,
-                R.id.notificationsFragment,
-                R.id.profileFragment,
-                R.id.searchFragment
-            )
+
+    private fun moveToFragments(fragment: Fragment){
+        val fragment_tranz =  supportFragmentManager.beginTransaction()
+        fragment_tranz.replace(
+            R.id.fragment_container ,
+            fragment
+
         )
-        setupActionBarWithNavController(controller, appBarConfig)
-        navView.setupWithNavController(controller)
+        fragment_tranz.commit()
+
+
     }
 
 }
