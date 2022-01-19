fec68d25191fc925d897621db4a7141fc865857e
davitbediashvili <davit.bediashvili.1@btu.edu.ge>
2022-01-19 23:06:24 +0400

    TheNew5.2(ანუ კი პოულობს მაგრამ იუზერნეიმს არ წერს და გამებურღა ტვინი)

--- app/src/main/java/com/example/myapplication/Adapter/UserAdapter.kt
+++ app/src/main/java/com/example/myapplication/Adapter/UserAdapter.kt
@@ -1,7 +1,6 @@
 package com.example.myapplication.Adapter
 
 import android.content.Context
-import android.text.Layout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,39 +12,31 @@ import com.example.myapplication.Model.User
 import com.example.myapplication.R
 import com.squareup.picasso.Picasso
 import de.hdodenhof.circleimageview.CircleImageView
-import org.w3c.dom.Text
 
-class UserAdapter(private var mcontext : Context,
-                  private var  mUser : List<User> ,
-                  private var isFragment : Boolean = false):RecyclerView.Adapter<UserAdapter.ViewHolder>(){
+class UserAdapter (private var mContext: Context,
+                   private var mUser: List<User>,
+                   private var isFragment: Boolean = false) : RecyclerView.Adapter<UserAdapter.ViewHolder>()
+{
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserAdapter.ViewHolder {
-
-        val view = LayoutInflater.from(mcontext).inflate(R.layout.user_info , parent ,false)
-
+        val view = LayoutInflater.from(mContext).inflate(R.layout.user_item_layout,parent,false)
         return UserAdapter.ViewHolder(view)
     }
 
-
-
+    override fun onBindViewHolder(holder: UserAdapter.ViewHolder, position: Int) {
+        val user = mUser[position]
+        holder.userNameTextView.text = user.getUsername()
+        Picasso.get().load(user.getimage()).placeholder(R.drawable.profile).into(holder.userProfileImage)
+    }
 
     override fun getItemCount(): Int {
         return mUser.size
-
-    }
-    override fun onBindViewHolder(holder: UserAdapter.ViewHolder, position: Int) {
-        val readUser = mUser[position]
-        holder.userNamee.text = readUser.getUserName()
-        holder.fullname.text = readUser.getFullName()
-        Picasso.get().load(readUser.getImage()).placeholder(R.drawable.profile).into(holder.image)
-
     }
 
-    class ViewHolder(@NonNull itemView : View) : RecyclerView.ViewHolder(itemView){
-
-         var userNamee : TextView = itemView.findViewById(R.id.username_searched)
-         var fullname : TextView = itemView.findViewById(R.id.username_fullname)
-         var image : CircleImageView = itemView.findViewById(R.id.user_image)
-         var followButton : Button = itemView.findViewById(R.id.follow_button)
-
+    class ViewHolder (@NonNull itemView: View) : RecyclerView.ViewHolder(itemView)
+    {
+        var userNameTextView: TextView = itemView.findViewById(R.id.user_full_name_search)
+        var userProfileImage: CircleImageView = itemView.findViewById(R.id.user_profile_image_search)
+        var followButton: Button = itemView.findViewById(R.id.followButtonSearch)
     }
+
 }
